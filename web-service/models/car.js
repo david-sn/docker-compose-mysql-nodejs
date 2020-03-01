6baa688af85722b1afe4ac1059d5544ca111850f
David Shiref <david.sn|@yahoo.com>
2020-03-01 15:49:34 +0200

    define details constraints model

--- web-service/models/car.js
+++ web-service/models/car.js
@@ -3,8 +3,17 @@ module.exports = (sequelize, DataTypes) => {
   const Cars = sequelize.define('Cars', {
     brandName: DataTypes.STRING,
     yearMade: DataTypes.INTEGER,
-    cost: DataTypes.DECIMAL
-  }, {});
+    cost: {
+      type: DataTypes.DECIMAL,
+      defaultValue: 0.0,
+      unique: false,
+      allowNull: false
+    }
+  }, {
+    timestamps: false,
+    freezTableName: false//sequlize plorilse table name by adding s at end of model name
+
+  });
   Cars.associate = function (models) {
     // associations can be defined here
     models.Cars.belongsTo(models.Customers, {
